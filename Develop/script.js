3cb118486f6b85c86230e924bf9dc37848b23a17
Stephen Marsh <stevemarsh117@gmail.com>
2021-02-04 17:44:06 -0500

    Added Variables

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var passwordLength = prompt("Choose a password length between: 8 & 128")
+  var options= ["abcdefghijklmnopqrstuvwxyz"]
+  var number
 
   passwordText.value = password;
 
@@ -12,3 +15,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// for (i = 0; i < 5; i++) {
+//   text += "The number is " + i + "<br>";
+// }
